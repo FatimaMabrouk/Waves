f4047d1092f6f67e6f611c14f38a924fec31b4d8
fatima mabrouk <onlyfefe03@gmail.com>
2020-01-14 23:40:57 +0100

    server Login users and create tokens

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ const  {User} = require("./models/user")
 //      USER - Router
 // ================================================
 
-app.post('/api/users/register', (req, res)=>{
+app.post("/api/users/register", (req, res)=>{
       const user = new User(req.body);
 
       user.save((err, doc)=>{
@@ -43,8 +43,30 @@ app.post('/api/users/register', (req, res)=>{
 });
 
 
+app.post("/api/users/login", (req, res)=> {
+ 
+    User.findOne({'email': req.body.email},(err, user)=> {
+        if(!user) return res.json({loginSuccess: false, message: "this email not found .."});
 
-const port = process.env.PORT || 3002;
-app.listen(port, ()=> {
-    console.log(`Server Running at ${port} `);
-})
+        user.comparePassword(req.body.password, (err, isMatch)=> {
+            if(!isMatch) return res.json({loginSuccess: false, message:"Wrong Password"});
+
+            user.generateToken( (err, user)=>{
+                if(err) return res.status(400).send(err);
+                res.cookie("w_auth", user.token).status(200).json({
+                    loginSuccess: true,
+                    message: user
+                })
+
+            })   
+        })
+    })
+
+});
+
+ 
+const port = process.env.PORT || 3002;   
+
+app.listen(port, (req, res)=> {
+    console.log("Server Running at : " + port)
+});
