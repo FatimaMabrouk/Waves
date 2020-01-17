41e9937e53bd6f2b0c366239ba8885cf14fb53d3
fatima mabrouk <onlyfefe03@gmail.com>
2020-01-17 18:17:34 +0100

    create dashboard for client

--- client/src/routes.js
+++ client/src/routes.js
@@ -5,11 +5,13 @@ import  Home  from './component/Home';
 import Layout from './hoc/Layout'
 import RegisterLogin from './component/Register_Login';
 import Register from './component/Register_Login/register';
+import UserDashbord from './component/User';
 
 const Routes = () => {
   return (
     <Layout> 
           <Switch>
+              <Route path="/user/dashboard"  exact component={UserDashbord} />
               <Route path="/register" exact component={Register} />
               <Route path="/RegisterLogin" exact component={RegisterLogin}/>
               <Route path="/" exact component={Home} />
