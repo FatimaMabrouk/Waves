1f85a1581654e42cd50b40a768b2c85014b71b5f
fatima mabrouk <onlyfefe03@gmail.com>
2020-01-19 00:31:09 +0100

    start with card component

---
+++ client/src/component/uitls/card_block.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const CardBlock = (props) => {
+    return (
+        <div className="card_block">
+           <div className="container">
+               {
+                   props.title ?
+                   <div className="title">
+                      {props.title}
+                   </div> 
+                   
+                   : null 
+               }
+               <div style={{
+                   display:'flex',
+                   flexWrap:'wrap'
+               }}>
+                   { renderCards(props.list)}
+               </div>
+           </div>
+        </div>
+    )
+}
+
+export default CardBlock;
