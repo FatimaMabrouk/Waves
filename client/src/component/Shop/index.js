a913462720f46f7cba33a439fe3310b8eb839613
fatima mabrouk <onlyfefe03@gmail.com>
2020-01-19 18:33:04 +0100

    showing cards with filters

--- client/src/component/Shop/index.js
+++ client/src/component/Shop/index.js
@@ -115,17 +115,21 @@ class Shop extends Component {
                         </div>
                         <div className="right">
                             <div className="shop_options">
-                                <div className="shope_grids clear">
+                                <div className="shop_grids clear">
                                     grids
                                 </div>
 
                             </div>
-                            <LoadmoreCards
-                             grid={this.state.grid}
-                             limit={this.state.limit}
-                             size={products.toShop}
-                             loadMore={ ()=> console.log('load more') }
+                            <div>
+                                <LoadmoreCards
+                                    grid={this.state.grid}
+                                    limit={this.state.limit}
+                                    size={products.toshopeSize} 
+                                    products={products.toShop}
+                                    loadMore={ ()=> console.log('load more') }
                             />
+                            </div>
+                            
                         </div>
                     </div>
                 </div>
--- client/src/component/Shop/loadmoreCards.js
+++ client/src/component/Shop/loadmoreCards.js
@@ -1,15 +1,41 @@
 import React  from 'react';
 import CardBlockShop from '../uitls/card_block_shop';
 
- const  LoadmoreCards = () => {
-    
-        return (
-            <div>
-                <CardBlockShop
-                grid={this.props}
-                list={props.list}
-                />
-            </div>
-        )
-}
+ const  LoadmoreCards = (props) => {
+   return (
+    <div>
+        <div>
+            <CardBlockShop
+                grid={props.grid}
+                list={props.products}
+            />
+        </div>
+        {/* {
+            props.size > 0 && props.size >= props.limit ?
+                <div className="load_more_container">
+                    <span onClick={()=> props.loadMore()}>
+                        Load More
+                    </span>
+                </div>
+            :null
+        } */}
+        
+
+
+    </div>
+);
+}; 
+     
+
 export default LoadmoreCards;
+
+
+// return (
+//     <div>
+//         <CardBlockShop
+//         grid={this.props}
+//         list={props.list}
+//         />
+//     </div>
+    
+// )
