6e836fbb60d51770a15537f8e49ccf74de1ed3df
fatima mabrouk <onlyfefe03@gmail.com>
2020-01-20 04:51:50 +0100

    product detailes c7

--- client/src/component/Product/productImg.js
+++ client/src/component/Product/productImg.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import  ImageLightBox from '../uitls/lightbox'
 
-export default class productImg extends Component {
+class productImg extends Component {
     state = {
         lightbox: false,
         imagePos:0,
@@ -21,13 +22,44 @@ export default class productImg extends Component {
         }
     }
 
+    renderCardImage = (images) => {
+        if(images.length > 0){
+            return images[0].url
+        }else{
+            return `/images/image_not_availble.png`
+        }
+    }
 
+
+   
     render() {
-        console.log(this.state)
+        const {detail} = this.props;
         return (
-            <div>
-                
+            <div className="product_image_container">
+                <div className="main_pic">
+                    <div
+                        style={{background:`url(${this.renderCardImage(detail.images)}) no-repeat`}} 
+                        onClick={()=> this.handleLightBox(0)}
+                    >
+                    </div>
+                </div>
+                <div className="main_thumbs">
+                    { this.showThumbs(detail)}
+                </div>
+                {
+                    this.state.lightbox ?
+                        <ImageLightBox
+                            id={detail.id}
+                            images={this.state.lightboxImages}
+                            open={this.state.open}
+                            pos={this.state.imagePos}
+                            onclose={()=> this.handleLightBoxClose()}
+                        />
+                    :null
+                }
             </div>
-        )
+        );
     }
 }
+
+export default  productImg;
